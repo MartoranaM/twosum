572314d10d380d143dffb05d0e6e73f4a43e4d8d
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-09-07 15:17:26 -0500

    Now does not allow same number used twice

--- twosum.cpp
+++ twosum.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 
 int main () {
-    std::vector <int> nums {2,7,11,15};
-    int target {26};
+    std::vector <int> nums {2,5,5,11};
+    int target {10};
     
     std::vector <int> newvec {};
     size_t size = nums.size();
     bool check = true;
     for (size_t i{0}; i<size && check == true; i++) {
         for (size_t j{1}; j<size; j++) {
-            int sum = nums[i] + nums[j];
-            if(sum == target) {
-                newvec.push_back(i);
-                newvec.push_back(j);
-                check = false;
+            if (i != j){
+                int sum = nums[i] + nums[j];
+                if(sum == target) {
+                    newvec.push_back(i);
+                    newvec.push_back(j);
+                    check = false;
+                }
             }
         }
     }
